9931acc2b7f888be498cf35cd3da6998afbbb064
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2023-07-02 16:49:34 -0300

    feat: setting up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { DetailsComponent } from './details/details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, title: 'Home page'},
+  {path: 'details/:id', component: DetailsComponent, title: 'Home details'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
