a90287fae2aaa1ca09021ae4eccc496964342123
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2023-07-03 08:25:41 -0300

    feat:  search functionality added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,27 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent implements OnInit {
 
-  housingLocationList: HousingLocation[]
+  housingLocationList: HousingLocation[];
+  filteredLocationList: HousingLocation[];
 
   constructor(private housingService: HousingService) {
-    this.housingLocationList = housingService.getAllHousingLoucations();
+    this.housingLocationList = this.housingService.getAllHousingLoucations();
+    this.filteredLocationList = this.housingLocationList;
    }
 
   ngOnInit(): void {
   }
+
+  filterResults(text: string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+
 
 }
